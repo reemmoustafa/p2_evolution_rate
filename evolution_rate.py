427efc52b294bd25e4de0637046aa165d5e3536d
Reem <August_21>
2019-04-27 22:27:08 +0200

    ask for input file from user and path validation

---
+++ evolution_rate.py
@@ -0,0 +1,20 @@
+import os
+
+"""A welcome message that briefly explains the aim of the script"""
+print('Welcome to the "Determining the rate of evolution of protein-coding sequences" script.' + '\n The purpose of the program is to determine the rate of evolution of protein coding sequences by calculating the dn/ds ratio of a certain gene among different species presented in a FASTA file.\n')
+
+
+fp_flag = 'false' #fp_flag variable used for file path validation
+while fp_flag is 'false':
+    # ask for file path as an input parameter from user
+    fp = input("Please enter the full path of your FASTA file that contains CDS sequences:  ")
+    # validate the existence of the file path with success message or failure message
+    try:
+        assert os.path.exists(fp)
+        #fp_flag = true
+        print('File ' + str(fp) + ' was found. ')
+        fp_flag = 'true' #flag is true if user entered correct file path
+    except AssertionError as e:
+        print('You have entered the following file path: ' + str(
+        fp) + '.'+ ' This file either does not exist or have a wrong extension.' + '\n Please try again with correct file path and file extension ".fasta"')
+        fp_flag = 'false' #fp_flag is false if file path is incorrect
