aefc12108fb7ad8fd3176912632021e9b101dc38
Reem <August_21>
2019-05-01 21:39:00 +0200

    delete unneeded files+ text file for code backup

--- functions.py
+++
@@ -1,7 +0,0 @@
-from Bio.SeqRecord import SeqRecord
-from Bio import SeqIO
-def make_prot_rec(nuc_rec):
-    """Returns a new SeqRecord with the translated sequence (default table)."""
-    return SeqRecord(seq = nuc_rec.seq.translate(cds=True),
-                     id = "trans_" + nuc_rec.id,
-                     description = "translation of CDS, using default table")
