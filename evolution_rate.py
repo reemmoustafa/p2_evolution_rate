d13fbf2d36e5ef68933c7a40f433386053f8962c
mina-saleh <49322405+mina-saleh@users.noreply.github.com>
2019-05-27 19:27:28 +0200

    :D adding spaces

--- evolution_rate.py
+++ evolution_rate.py
@@ -1,17 +1,28 @@
+# copy rights for: Reem Mostafa, Nihal, Mina S. A. Saleh, Manar M. Rashed, Abeer, Under supervision of: Dr. Rosina
+# This algorithm is part of Programing Course, Bioinformatics Diploma, Nile University, Cairo, Egypt
+#
+
+
 import os
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 from Bio.Align.Applications import MuscleCommandline
-#from io import StringIO
-#from Bio import AlignIO
+# from io import StringIO
+# from Bio import AlignIO
+
+
 def make_prot_rec(nuc_rec):
     """function that returns a new SeqRecord with the translated sequence (default table)."""
-    return SeqRecord(seq = nuc_rec.seq.translate(cds=True),
-                     id = "trans_" + nuc_rec.id,
-                     description = "translation of CDS, using default table")
+    return SeqRecord(seq=nuc_rec.seq.translate(cds=True),
+                     id="trans_" + nuc_rec.id,
+                     description="translation of CDS, using default table")
+
+
 # A welcome message that briefly explains the aim of the script
 print(
-    'Welcome to the "Determining the rate of evolution of protein-coding sequences" script.' + '\nThe purpose of the program is to determine the rate of evolution of protein coding sequences\nby calculating the dn/ds ratio of a certain gene among different species presented in a FASTA file.')
+    'Welcome to the "Determining the rate of evolution of protein-coding sequences" script.'
+    + '\nThe purpose of the program is to determine the rate of evolution of protein coding sequences'
+      '\nby calculating the dn/ds ratio of a certain gene among different species presented in a FASTA file.')
 print('*' * 100)
 # subtask 0: ask user for input fasta file + file validation for the entered
 # path and file extension(this subtask
@@ -38,44 +49,43 @@ while fpath_flag is 'false':
         fpath_flag = 'false'  # fpath_flag is false if file path is incorrect
 
 # Sub-task 1: parse input file
-#codon_table = CodonTable.ambiguous_dna_by_id[1]
-with open(fpath) as file:  #with as method for proper handling of large files
+# codon_table = CodonTable.ambiguous_dna_by_id[1]
+with open(fpath) as file:  # with as method for proper handling of large files
     # regardless of the Operating system styling
     # file: a parameter that will be used as a handle
-#subtask 1 + 2: continue with file parsing + validation of coding sequences only
+    # subtask 1 + 2: continue with file parsing + validation of coding sequences only
     print("Validating the input file for the presence of complete coding sequences only")
-    for record in SeqIO.parse(file, "fasta"): #for loop to parse the input file
+    for record in SeqIO.parse(file, "fasta"):  # for loop to parse the input file
         # record: variable of type SeqRecord object
-        gene_seq = record.seq #gene_seq: is a variable of type seq object
+        gene_seq = record.seq  # gene_seq: is a variable of type seq object
         try:
             gene_seq.translate(cds=True)
-        #parameter cds - Boolean, indicates this is a complete CDS.
+        # parameter cds - Boolean, indicates this is a complete CDS.
         # If True, this checks the sequence starts with a valid alternative start
         # codon (which will be translated as methionine, M), that the sequence
         # length is a multiple of three, and that there is a single in frame stop
         # codon at the end. If these tests fail, an exception is raised.
         except Exception as cds_error:
             print('Error in Sequence id '+str(record.id)+':\n\t'+str(cds_error)+'\n\t'
-                  +str(gene_seq)) #print of sequence id + exact error + coding sequence
-            print('\tUnfortunately, The Program will terminate now.') #exit message for the user
-            exit()#program will crash and exit as required
+                  + str(gene_seq))  # print of sequence id + exact error + coding sequence
+            print('\tUnfortunately, The Program will terminate now.')  # exit message for the user
+            exit()  # program will crash and exit as required
     print("File contains complete CDS only. File is accepted")
-#subtask 3: Convert the coding sequences to protein sequences by translation
+# subtask 3: Convert the coding sequences to protein sequences by translation
 with open(fpath) as file:
-    fname_prot = "protSeq_" + fname #fname_prot: variable of the protein file name (translation step output)
-    #translate neucleotide seqrecords to protein seqrecords and store it in an output file
+    fname_prot = "protSeq_" + fname  # fname_prot: variable of the protein file name (translation step output)
+    # translate neucleotide seqrecords to protein seqrecords and store it in an output file
     proteins = (make_prot_rec(nuc_rec) for nuc_rec in SeqIO.parse(file, "fasta"))
     SeqIO.write(proteins, fname_prot, "fasta")
-#subtask 4: Align the protein sequences using MUSCLE program
-#fn_p_muscle = "Alg_"+fname_prot
+# subtask 4: Align the protein sequences using MUSCLE program
+# fn_p_muscle = "Alg_"+fname_prot
 muscle_exe = "muscle3.8.31_i86win32.exe"
-#muscle_exe: variable containing the path of muscle program
-fname_prot_musc_in = fname_prot #variable: input file for muscle
-fname_prot_musc_out = "Alg_" + fname_prot_musc_in #variable: output file from muscle
-muscle_cline = MuscleCommandline(muscle_exe, input=fname_prot_musc_in , out = fname_prot_musc_out)
+# muscle_exe: variable containing the path of muscle program
+fname_prot_musc_in = fname_prot  # variable: input file for muscle
+fname_prot_musc_out = "Alg_" + fname_prot_musc_in  # variable: output file from muscle
+muscle_cline = MuscleCommandline(muscle_exe, input=fname_prot_musc_in, out=fname_prot_musc_out)
 # variable for muscle commandline
-print(muscle_cline) #print statement of the commandline variable
-stdout, stderr = muscle_cline() #stdout, stderr runs muscle command variable
-#align = AlignIO.read(StringIO(stdout), "fasta")
-#print(align)
-
+print(muscle_cline)  # print statement of the commandline variable
+stdout, stderr = muscle_cline()  # stdout, stderr runs muscle command variable
+# align = AlignIO.read(StringIO(stdout), "fasta")
+# print(align)
