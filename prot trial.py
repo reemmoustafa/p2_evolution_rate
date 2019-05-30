82d0d795af3f018c66bd0395230f0b09d3fd52ae
mina-saleh <49322405+mina-saleh@users.noreply.github.com>
2019-05-31 01:04:48 +0200

    prot trial using files

--- prot trial.py
+++ prot trial.py
@@ -1,5 +1,17 @@
-alg_protein = 'MEILW-KTLTWILSL-IMASSEFYSDNRLSYSSQ---EEFLTYLEHYQLT-PIXXXXXXXXXXXXXXXXXXXXXX-XXXXX----X-----XX--XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXW'
-unalg_nuc_seq = "ATGGAAATTTTGTGGAAGACGTTGACCTGGATTTTGAGCCTCATCATGGCATCATCAGAATTTTATAGTGATAACAGGCTTTCATACAGTTCTCAAGAGGAATTCCTGACATATCTTGAACACTACCAGCTAACTCCAATANNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNTGG"
+
+from Bio import SeqIO
+
+
+nuc_inFile = open(r'C:\Users\Mina Saleh\PycharmProjects\p2_evolution_rate\Human_ADAMTS6_orthologues_2.fa','r')
+prot_inFile = open(r'C:\Users\Mina Saleh\PycharmProjects\p2_evolution_rate\Alg_protSeq_Human_ADAMTS6_orthologues_2.fa','r')
+
+for n_record in SeqIO.parse(nuc_inFile, "fasta"):
+    unalg_nuc_seq = n_record.seq
+#  r = str(gene_seq)
+# print(r)
+for p_record in SeqIO.parse(prot_inFile, "fasta"):
+    alg_protein = p_record.seq
+
 alg_nuc_seq: str = ''
 print(alg_nuc_seq)
 i = 0
