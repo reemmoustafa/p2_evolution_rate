a8b85c729718532a90a3f115d214b4c1316e6767
Reem <August_21>
2019-05-30 22:04:23 +0200

    Merge branch 'master' of E:\NU_Bio_diploma\CIT-656_Python\final_project\p2_evolution_rate with conflicts.

---
+++ txt2phylip.py
@@ -0,0 +1,13 @@
+#https://www.biostars.org/p/327003/"""
+#from Bio import AlignIO
+#from Bio import SeqIO
+#alignments = AlignIO.parse("Alg_protSeq_Human_ADAMTS6_orthologues_2.fa", "fasta")
+#with open("example.faa", "w") as handle:
+ #   count = SeqIO.write(alignments, handle, "fasta")
+from Bio import SeqIO
+from Bio import AlignIO
+#with open("example.faa", "w") as handle:
+ #   for record in AlignIO.parse("xA.fa", "fasta"):
+  #      count = SeqIO.write(record, handle, "phylip")
+count = AlignIO.convert("x.fa", "fasta", "example.phy", "phylip-relaxed")
+print("Converted %i alignments" % count)
