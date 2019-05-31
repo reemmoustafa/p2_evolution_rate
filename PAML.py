cb0eb36d5cb01959939ea1f351bc3f1bc571feb1
ManarRashad <48625774+ManarRashad@users.noreply.github.com>
2019-05-31 20:43:55 +0200

    Add files via upload

---
+++ PAML.py
@@ -0,0 +1,15 @@
+from Bio.Phylo.PAML import codeml
+cml = codeml.Codeml()
+cml.alignment = "Alg_NucSeq_PY_project_2_input_SRSF1.phy"
+cml.tree = "project2.newick"
+cml.out_file = "results_final.out"
+cml.working_dir = "C:/Users/DELL/Desktop/PAML_newdata/final_PAML"
+cml.set_options(CodonFreq=2, model=0, seqtype=1, NSsites='0', runmode=0, cleandata=0)
+results = cml.run()
+ns_sites = results.get("NSsites")
+m0 = ns_sites.get(0)
+m0_params = m0.get("parameters")
+print(m0_params.get("omega"))
+
+
+
